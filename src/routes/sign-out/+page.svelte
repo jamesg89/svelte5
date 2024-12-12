80ec2058bd1e813726cc717f9333e09231aab43c
jamesg89 <90154865+jamesg89@users.noreply.github.com>
2024-12-12 14:13:23 -0800

    Supplemental Auth

---
+++ src/routes/sign-out/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import AwardSection from '$lib/components/slices/AwardSection.svelte';
+    import ByTheNumbers from '$lib/components/slices/ByTheNumbers.svelte';
+    import CoreValues from '$lib/components/slices/CoreValues.svelte';
+    import { pageNameStore } from '$lib/stores';
+    
+    // Set the page name
+    $pageNameStore = 'Home';
+
+    let { data }: { data: PageData } = $props();
+</script>
+
+<svelte:head>
+    <title>Your Page Title</title>
+    <meta name="description" content="Your description" />
+    <meta property="og:title" content="Your OG Title" />
+</svelte:head>
+
+<div class="section"><p>You done been logged out sucka foo!</p></div>
+
+<AwardSection bg_color="White" />
+<ByTheNumbers bg_color="Gray" />
+<CoreValues bg_color="Slate" />
