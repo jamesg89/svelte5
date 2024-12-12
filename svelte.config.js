e09954e790a638dcc3ada3470509912f5edc7370
jamesg89 <90154865+jamesg89@users.noreply.github.com>
2024-12-11 22:18:41 -0800

    CF patch 5

--- svelte.config.js
+++ svelte.config.js
@@ -8,19 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
+		adapter: adapter()
 	}
 };
 
