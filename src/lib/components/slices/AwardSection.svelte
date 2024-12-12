bddc02d446b7244a6349b251bdb51cdb46c78220
jamesg89 <90154865+jamesg89@users.noreply.github.com>
2024-12-11 22:50:41 -0800

    Award Section Component Added

---
+++ src/lib/components/slices/AwardSection.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { clsx } from 'clsx';
+    import Oakwyn from '../Oakwyn.svelte';
+    import Tmg from '../Tmg.svelte';
+
+    export let bg_color: "White" | "Slate" | "Gray" = "White";
+</script>
+
+<section class="{clsx('', bg_color=== "White" ? "bg-white" : bg_color === 'Slate' ? 'bg-slate' :  bg_color === 'Gray' ? 'bg-gray-default' :'bg-white')}">
+		<div class="container flex flex-col relative z-10 justify-between items-center h-full text-center">  
+            <div class="mt-8 md:mt-0 w-full">
+				<h2 class="text-5xl font-bold mb-12">	
+					AWARD WINNING SERVICE
+				</h2>
+			        <!-- Oakwyn -->
+					<div class="flex flex-col md:flex-row gap-10 w-full items-center justify-center lg:justify-between px-0 mx-auto flex-wrap align-bottom">
+						    <div class="flex flex-1 flex-col gap-4 align-bottom text-center min-w-44 max-w-48">
+								<span class="max-h-48 max-w-48 text-center m-auto">
+                                    <Oakwyn />
+                                </span>
+								<p>	
+									Oakwyn Realty Top Producer
+								</p>
+							</div>
+
+                    <!-- Medallion -->
+                            <div class="flex flex-1 flex-col gap-4 align-bottom text-center min-w-44 max-w-48">
+								<span class="max-h-48 max-w-48 text-center m-auto">
+                                    <img src="/static/assets/big-medallion-club-vancouver-award-300x300.png" alt="fancy award for top 10% of all Realtors" />
+                                </span>
+								<p>	
+									Top 10% of Greater Vancouver Realtor
+								</p>
+							</div>
+
+                    <!-- TMG -->
+                            <div class="flex flex-1 flex-col gap-4 align-bottom text-center min-w-44 max-w-48">
+								<span class="max-h-48 max-w-48 text-center m-auto">
+                                    <Tmg />
+                                </span>
+								<p>	
+									Million Dollar Mortgage Club
+								</p>
+							</div>
+					</div>
+			</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type { PageData } from './$types';
-    import Hero from '$lib/components/slices/Hero.svelte';
+    import AwardSection from '$lib/components/slices/AwardSection.svelte';
     import { pageNameStore } from '$lib/stores';
     
     // Set the page name
@@ -14,3 +14,5 @@
     <meta name="description" content="Your description" />
     <meta property="og:title" content="Your OG Title" />
 </svelte:head>
+
+<AwardSection bg_color="White" />
